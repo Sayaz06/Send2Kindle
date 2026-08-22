0f1671cc2ee3e9791fbdd4dab616d75c7caef18e
Sayaz06 <adha86614@gmail.com>
2026-08-22 20:27:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kindle-queue-v15';
+const CACHE = 'kindle-queue-v16';
 const ASSETS = ['./', './index.html', './css/style.css', './manifest.json', './icons/icon.svg', './js/app.js'];
 
 self.addEventListener('install', e => {
